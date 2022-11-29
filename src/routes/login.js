0b829e33bb7ce7e5dd63e950c1c576043e7aaa0c
JPGarcia1 <104031764+JPGarcia1@users.noreply.github.com>
2022-11-29 08:58:15 -0500

    Delete src directory

--- src/routes/login.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const loginController = require('../controllers/loginController');
-
-const router = express.Router();
-
-router.get('/login', loginController.login);
-router.get('/register', loginController.register);
-
-module.exports = router;
